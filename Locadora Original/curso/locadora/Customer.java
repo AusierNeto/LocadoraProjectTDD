cddedfed1fff592b396492124625d44418bf9ce6
ausier <ausierneto@gmail.com>
2024-03-13 21:02:43 -0400

    Refactor: Customer Class

--- Locadora Original/curso/locadora/Customer.java
+++ Locadora Original/curso/locadora/Customer.java
@@ -7,40 +7,49 @@ public class Customer {
 	
 	private String _name;
 	private Vector<Rental> _rentals = new Vector<Rental>();
+	private int frequentRenterPoints;
+	private double totalAmount;
+	
 
 	public Customer(String name) {
 		_name = name;
 	};
 
-	public void addRental(Rental arg) {
-		_rentals.addElement(arg);
+	public void addRental(Rental rental) {
+		_rentals.addElement(rental);
+		frequentRenterPoints += rental.getFrequentRenterPoints();
+		totalAmount += rental.getAmount();
 	}
 
 	public String getName() {
 		return _name;
 	}
+	
+	private double getTotalAmount() {
+		return totalAmount;
+	}
+	
+	private int getFrequentRenterPoints() {
+		return frequentRenterPoints;
+	}
 
 	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
 		Enumeration<Rental> rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
 		
 		while (rentals.hasMoreElements()) {
 			Rental each = (Rental) rentals.nextElement();
-			double thisAmount = each.getAmount();
-			frequentRenterPoints += each.getFrequentRenterPoints();
 			
 			//show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+					+ String.valueOf(totalAmount) + "\n";
 		}
 		
 		//add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
+		result += "Amount owed is " + String.valueOf(getTotalAmount()) + "\n";
+		result += "You earned " + String.valueOf(getFrequentRenterPoints())
 				+ " frequent renter points";
 		return result;
 	}
+	
 }
